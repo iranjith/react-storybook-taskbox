e425c7694221503865a0ed131f10550e848dce7f
iranjith <rgranjith89@gmail.com>
2024-05-05 15:06:44 +0530

    Created 3 basic stories

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,15 +1,48 @@
-import React from 'react'
+import React from "react";
 
-const Task = ({task: {id, title, state}, onArchiveTask, onPinnedTask}) => {
+const Task = ({ task: { id, title, state }, onArchiveTask, onPinnedTask }) => {
   return (
     <>
-        <div className="list-item">
-            <label htmlFor='title' aria-label='title'>
-                <input type="text" value={title} readOnly={true} name='title' />
-            </label>
-        </div>
-    </>
-  )
-}
+      <div className={`list-item ${state}`}>
+        <label
+          htmlFor="checked"
+          className="checkBox"
+          aria-label={`archiveTask-${id}`}
+        >
+          <input
+            type="checkbox"
+            disabled={true}
+            name="checked"
+            id={`archiveTask-${id}`}
+            checked={state === "TASK_ARCHIVED"}
+          />
+          <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
+        </label>
 
-export default Task
+        <label htmlFor="title" aria-label={title} className="title">
+          <input
+            type="text"
+            value={title}
+            readOnly={true}
+            name="title"
+            placeholder="Input title"
+          />
+        </label>
+
+        {state !== "TASK_ARCHIVED" && (
+          <button
+            className="pin-button"
+            onClick={() => onPinTask(id)}
+            id={`pinTask-${id}`}
+            aria-label={`pinTask-${id}`}
+            key={`pinTask-${id}`}
+          >
+            <span className={`icon-star`} />
+          </button>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Task;
