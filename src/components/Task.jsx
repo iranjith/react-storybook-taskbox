8080e852a97a71b9a9533d5461cb1996b07d8dd4
iranjith <rgranjith89@gmail.com>
2024-05-05 15:10:55 +0530

    Added proptypes and modified CSS

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Task = ({ task: { id, title, state }, onArchiveTask, onPinnedTask }) => {
   return (
@@ -43,6 +44,23 @@ const Task = ({ task: { id, title, state }, onArchiveTask, onPinnedTask }) => {
       </div>
     </>
   );
+};
+
+//No need to use if we are using TypeScript
+Task.propTypes = {
+  /** Composition of the task */
+  task: PropTypes.shape({
+    /** Id of the task */
+    id: PropTypes.string.isRequired,
+    /** Title of the task */
+    title: PropTypes.string.isRequired,
+    /** Current state of the task */
+    state: PropTypes.string.isRequired,
+  }),
+  /** Event to change the task to archived */
+  onArchiveTask: PropTypes.func,
+  /** Event to change the task to pinned */
+  onPinTask: PropTypes.func,
 };
 
 export default Task;
